eb7a7541bbd0c85a5ab86885d5a0cf27d3a3275c
thomaskoji <abethomaskoji@gmail.com>
2017-01-31 17:44:16 -0500

    Add files via upload

---
+++ week1/js/sketch.js
@@ -0,0 +1,9 @@
+function setup(){
+    createCanvas(800,800);
+    line(800, 800, 50, 50);
+    ellipse(300,300,600);
+    ellipse();
+    ellipse();
+
+    //two points, numbers in (x1, y1, x2, y2)
+}
